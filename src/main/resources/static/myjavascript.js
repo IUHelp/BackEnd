5dedb836a0cb81dfcc4bf7e77a9aa85d3f02bdc4
prashanth bheemagani <pbheemag@indiana.edu>
2016-12-12 12:39:26 -0500

    verifying text result

--- src/main/resources/static/myjavascript.js
+++ src/main/resources/static/myjavascript.js
@@ -353,7 +353,7 @@ function httpGetAsyncText() {
                                                            }
                                                        console.log("main content---------------------------");
                                                        console.log(maincontent);
-                                            $('<div class="res"><div class="container-fluid"><div class="collapse" id ="info">'+maincontent+'</div></div></div><br>').appendTo('#SearchResult');
+                                            $('<div class="res"><div class="container-fluid"><div id ="info">'+maincontent+'</div></div></div><br>').appendTo('#SearchResult');
                                        }
 
                                        link = response.linksResult[i];
